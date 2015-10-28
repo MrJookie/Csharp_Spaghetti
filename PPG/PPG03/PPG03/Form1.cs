34b49631e0a213c97f8630ccf252b385ceab4540
MrJookie <black.lemon1@gmail.com>
2015-10-28 16:10:34 +0100

    Fixed Quadtree

--- PPG/PPG03/PPG03/Form1.cs
+++ PPG/PPG03/PPG03/Form1.cs
@@ -21,18 +21,25 @@ namespace PPG03
             Bitmap b = new Bitmap("C:\\Users\\admin\\Desktop\\grafika\\cb.png");
             Graphics g = this.CreateGraphics();
 
-            //g.DrawImage(b, 0, 40);
+            g.DrawImage(b, 0, 40);
 
-            quadtree(b, 0, 0, b.Width, b.Height);
-            //rle(b);
+            string resultQuadtree = quadtree(b, 0, 0, b.Width, b.Height);
+            Console.WriteLine(resultQuadtree);
 
-            //string[] lines = { Convert.ToString(b.Width), Convert.ToString(b.Height) };
-            //System.IO.File.WriteAllLines(@"C:\\Users\\admin\\Desktop\\grafika\\quadtree.txt", lines);
+            string resultRLE = rle(b);
+            Console.WriteLine(resultRLE);
+
+            string[] resultQuadtreeToFile = new string[] { resultQuadtree };
+            System.IO.File.WriteAllLines(@"C:\\Users\\admin\\Desktop\\grafika\\quadtree.txt", resultQuadtreeToFile);
+
+            string[] resultRLEToFile = new string[] { resultRLE };
+            System.IO.File.WriteAllLines(@"C:\\Users\\admin\\Desktop\\grafika\\rle.txt", resultRLEToFile);
         }
 
-        private void rle(Bitmap b)
+        private string rle(Bitmap b)
         {
             Color c1 = b.GetPixel(0, 0);
+            string output = "";
 
             for (int y = 0; y < b.Height; y++)
             {
@@ -52,48 +59,56 @@ namespace PPG03
                     }
                 }
 
-                Console.WriteLine("("+colorCounter+",C1), ("+colorCounter2+",C2)");
+                //Console.WriteLine("("+colorCounter+",C1), ("+colorCounter2+",C2)");
+                output += "((" + colorCounter + ",R)(" + colorCounter2 + ",W))\n";
             }
+
+            return output;
         }
 
-        //to fix
         private string quadtree(Bitmap b, int Xmin, int Ymin, int Xmax, int Ymax)
         {
             Color color1 = b.GetPixel(Xmin, Ymin);
 
-            bool differentColor = true;
+            string color;
+            if (color1.R == 255 && color1.G == 255 && color1.B == 255)
+                color = "W";
+            else
+                color = "R";
 
-            for (int x = Xmin; x < Xmax; x++)
+            bool uniColor = true;
+
+            for (int x = Xmin; x < Xmin + Xmax; x++)
             {
-                for (int y = Ymin; y < Ymax; y++)
+                for (int y = Ymin; y < Ymin + Ymax; y++)
                 {
                     Color color2 = b.GetPixel(x, y);
                     if (color2 != color1)
                     {
-                        differentColor = true;
+                        uniColor = false;
                         break;
                     }
-
-                    if (differentColor == false)
-                    {
-                        return "different color";
-                        //return Convert.ToString(soucasnaBarva);
-                    }
-
-                    string output = "(";
-                    output += quadtree(b, Xmin + Xmax / 2, Ymin, Xmax / 2, Ymax / 2);
-                    output += quadtree(b, Xmin, Ymin, Xmax / 2, Ymax / 2);
-                    output += quadtree(b, Xmin, Ymin + Ymax / 2, Xmax / 2, Ymax / 2);
-                    output += quadtree(b, Xmin + Xmax / 2, Ymin + Ymax / 2, Xmax / 2, Ymax / 2);
-                    output += ")";
-
-                    Console.WriteLine(output);
-
-                    return output;
                 }
             }
 
-            return "*"; //?
+            if (uniColor)
+            {
+                //Console.WriteLine(color);
+                return color;
+            }
+            else
+            {
+                string output = "(";
+                output += quadtree(b, Xmin, Ymin, Xmax / 2, Ymax / 2);
+                output += quadtree(b, Xmin + Xmax / 2, Ymin, Xmax / 2, Ymax / 2);
+                output += quadtree(b, Xmin, Ymin + Ymax / 2, Xmax / 2, Ymax / 2);
+                output += quadtree(b, Xmin + Xmax / 2, Ymin + Ymax / 2, Xmax / 2, Ymax / 2);
+                output += ")";
+
+                //Console.WriteLine(output);
+                return output;
+            }
         }
+
     }
 }
